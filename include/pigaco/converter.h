079db7bc01d9ceef91d5538d60db80d0358d8b1c
DecuBan <salimovabdurauf@gmail.com>
2025-03-14 00:26:32 +0500

    ...

--- include/pigaco/converter.h
+++ include/pigaco/converter.h
@@ -1,20 +1,22 @@
 #ifndef PIGACO_CONVERTER_H
 #define PIGACO_CONVERTER_H
 
-#include <malloc.h>
-#include <math.h>
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <wchar.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "pigaco/stb_image.h"
+#ifndef PGDEF
+#ifdef PG_CONVERTER_STATIC
+#define PGDEF static
+#else
+#define PGDEF extern
+#endif
+#endif
+
+#define PG_VERSION 1
 
 #define ASCII_CHARS L" .,:;irsXA253hMHGS#9B&@"
 #define ASCII_CHARS_LEN ((sizeof(ASCII_CHARS) / sizeof(wchar_t)) - 1)
+
+typedef unsigned int pgu8;
 
 struct Image {
   int width;
@@ -41,7 +43,189 @@ typedef struct {
 extern "C" {
 #endif // __cplusplus
 
-static void apply_contrast(unsigned char *gray, int width, int height,
+PGDEF void apply__contrast(unsigned char *gray, int width, int height,
+                           float contrast);
+
+PGDEF void floyd__steinberg_dither(unsigned char *gray, int width, int height);
+
+PGDEF void *process__rows(void *arg);
+
+PGDEF void convert_image_to_ascii(const char *filename, int scale,
+                                  float aspect_ratio);
+
+PGDEF const pgu8 pg_version();
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // PIGACO_CONVERTER_H
+
+/*
+ * end of header file
+ * * * * * * * * * * * * * * * * * */
+
+#ifdef PG_CONVERTER_IMPLEMENTATION
+
+#include <math.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <wchar.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
+#define PG_MALLOC(size) malloc(size)
+#define PG_FREE(ptr) free(ptr)
+
+#ifdef __cplusplus
+#define pg_inline inline
+#else
+#define pg_inline
+#endif
+
+PGDEF void convert_image_to_ascii(const char *filename, int scale,
+                                  float aspect_ratio) {
+  struct Image *image = (struct Image *)PG_MALLOC(sizeof(struct Image));
+
+  image->data =
+      stbi_load(filename, &image->width, &image->height, &image->channels, 3);
+
+  if (!image) {
+    fwprintf(stderr, L"%s\n", stbi_failure_reason());
+
+    return;
+  }
+
+  unsigned char *gray = PG_MALLOC(image->width * image->height);
+  if (!gray) {
+    stbi_image_free(image->data);
+
+    wprintf(L"Error allocate memory for gray.\n");
+
+    return;
+  }
+
+  for (int y = 0; y < image->height; y++) {
+    for (int x = 0; x < image->width; x++) {
+      int idx = (y * image->width + x) * 3;
+
+      unsigned char r = image->data[idx];
+      unsigned char g = image->data[idx + 1];
+      unsigned char b = image->data[idx + 2];
+
+      float lum = 0.299f * r + 0.587f * g + 0.114f * b;
+
+      gray[y * image->width + x] = (unsigned char)lum;
+    }
+  }
+
+  float contrast_factor = 1.1f;
+  apply__contrast(gray, image->width, image->height, contrast_factor);
+
+  floyd__steinberg_dither(gray, image->width, image->height);
+
+  /* int scale = 8; */
+  /* float aspect_ratio = 0.5f; */
+  int vscale = (int)(scale / aspect_ratio);
+
+  if (vscale < 1)
+    vscale = 1;
+
+  int use_color = 1;
+
+  int out_rows = (image->height + vscale - 1) / vscale;
+  int out_cols = (image->width + scale - 1) / scale;
+
+  int num_threads = 1;
+#ifdef _SC_NPROCESSORS_ONLN
+  long cpus = sysconf(_SC_NPROCESSORS_ONLN);
+  if (cpus > 1)
+    num_threads = (int)cpus;
+#endif
+  wprintf(L"Using %d thread(s)\n", num_threads);
+
+  wchar_t **output = PG_MALLOC(out_rows * sizeof(wchar_t *));
+  if (!output) {
+    wprintf(L"Error allocate memory for output.\n");
+
+    PG_FREE(gray);
+
+    stbi_image_free(image->data);
+
+    PG_FREE(image);
+
+    return;
+  }
+
+  memset(output, 0, out_rows * sizeof(wchar_t *));
+
+  pthread_t *threads = PG_MALLOC(num_threads * sizeof(pthread_t));
+  ThreadData *thread_data = PG_MALLOC(num_threads * sizeof(ThreadData));
+  if (!threads || !thread_data) {
+    fwprintf(stderr, L"Error allocate memory for threads.\n");
+
+    PG_FREE(output);
+    PG_FREE(gray);
+
+    stbi_image_free(image->data);
+
+    PG_FREE(image);
+
+    return;
+  }
+
+  int rows_per_thread = out_rows / num_threads;
+  int extra_rows = out_rows % num_threads;
+  int current_row = 0;
+
+  for (int i = 0; i < num_threads; i++) {
+    int start = current_row;
+    int add = (i < extra_rows) ? 1 : 0;
+    int end = start + rows_per_thread + add;
+
+    thread_data[i].start_row = start;
+    thread_data[i].end_row = end;
+    thread_data[i].out_cols = out_cols;
+    thread_data[i].width = image->width;
+    thread_data[i].height = image->height;
+    thread_data[i].scale = scale;
+    thread_data[i].vscale = vscale;
+    thread_data[i].use_color = use_color;
+    thread_data[i].image = image->data;
+    thread_data[i].gray = gray;
+    thread_data[i].out = output;
+    current_row = end;
+
+    if (pthread_create(&threads[i], NULL, process__rows, &thread_data[i]) != 0)
+      fwprintf(stderr, L"Error create thread %d\n", i);
+  }
+
+  for (int i = 0; i < num_threads; i++)
+    pthread_join(threads[i], NULL);
+
+  for (int i = 0; i < out_rows; i++) {
+    if (output[i]) {
+      wprintf(L"%ls\n", output[i]);
+
+      PG_FREE(output[i]);
+    }
+  }
+
+  PG_FREE(output);
+  PG_FREE(threads);
+  PG_FREE(thread_data);
+  PG_FREE(gray);
+
+  stbi_image_free(image->data);
+
+  PG_FREE(image);
+}
+
+PGDEF void apply__contrast(unsigned char *gray, int width, int height,
                            float contrast) {
   int size = width * height;
   for (int i = 0; i < size; i++) {
@@ -56,7 +240,7 @@ static void apply_contrast(unsigned char *gray, int width, int height,
   }
 }
 
-static void floyd_steinberg_dither(unsigned char *gray, int width, int height) {
+PGDEF void floyd__steinberg_dither(unsigned char *gray, int width, int height) {
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
       int idx = y * width + x;
@@ -84,7 +268,7 @@ static void floyd_steinberg_dither(unsigned char *gray, int width, int height) {
   }
 }
 
-static void *process_rows(void *arg) {
+PGDEF void *process__rows(void *arg) {
   ThreadData *data = (ThreadData *)arg;
 
   for (int out_y = data->start_row; out_y < data->end_row; out_y++) {
@@ -98,7 +282,7 @@ static void *process_rows(void *arg) {
     else
       buffer_size = data->out_cols + 1;
 
-    wchar_t *line = malloc(buffer_size * sizeof(wchar_t));
+    wchar_t *line = PG_MALLOC(buffer_size * sizeof(wchar_t));
     if (!line) {
       fwprintf(stderr, L"Error allocate memory for line %d\n", out_y);
       continue;
@@ -135,145 +319,6 @@ static void *process_rows(void *arg) {
   return NULL;
 }
 
-static void convert_image_to_ascii(const char *filename) {
-  struct Image *image = (struct Image *)malloc(sizeof(struct Image) + 16);
+PGDEF const pgu8 pg_version() { return PG_VERSION; }
 
-  image->data =
-      stbi_load(filename, &image->width, &image->height, &image->channels, 3);
-
-  if (!image) {
-    fwprintf(stderr, L"%s\n", stbi_failure_reason());
-
-    return;
-  }
-
-  unsigned char *gray = malloc(image->width * image->height);
-  if (!gray) {
-    stbi_image_free(image->data);
-
-    wprintf(L"Error allocate memory for gray.\n");
-
-    return;
-  }
-
-  for (int y = 0; y < image->height; y++) {
-    for (int x = 0; x < image->width; x++) {
-      int idx = (y * image->width + x) * 3;
-
-      unsigned char r = image->data[idx];
-      unsigned char g = image->data[idx + 1];
-      unsigned char b = image->data[idx + 2];
-
-      float lum = 0.299f * r + 0.587f * g + 0.114f * b;
-
-      gray[y * image->width + x] = (unsigned char)lum;
-    }
-  }
-
-  float contrast_factor = 1.1f;
-  apply_contrast(gray, image->width, image->height, contrast_factor);
-
-  floyd_steinberg_dither(gray, image->width, image->height);
-
-  int scale = 8;
-  float aspect_ratio = 0.5f;
-  int vscale = (int)(scale / aspect_ratio);
-
-  if (vscale < 1)
-    vscale = 1;
-
-  int use_color = 1;
-
-  int out_rows = (image->height + vscale - 1) / vscale;
-  int out_cols = (image->width + scale - 1) / scale;
-
-  int num_threads = 1;
-#ifdef _SC_NPROCESSORS_ONLN
-  long cpus = sysconf(_SC_NPROCESSORS_ONLN);
-  if (cpus > 1)
-    num_threads = (int)cpus;
-#endif
-  wprintf(L"Using %d thread(s)\n", num_threads);
-
-  wchar_t **output = malloc(out_rows * sizeof(wchar_t *));
-  if (!output) {
-    wprintf(L"Error allocate memory for output.\n");
-
-    free(gray);
-
-    stbi_image_free(image->data);
-
-    free(image);
-
-    return;
-  }
-
-  memset(output, 0, out_rows * sizeof(wchar_t *));
-
-  pthread_t *threads = malloc(num_threads * sizeof(pthread_t));
-  ThreadData *thread_data = malloc(num_threads * sizeof(ThreadData));
-  if (!threads || !thread_data) {
-    fwprintf(stderr, L"Error allocate memory for threads.\n");
-
-    free(output);
-    free(gray);
-
-    stbi_image_free(image->data);
-
-    free(image);
-
-    return;
-  }
-
-  int rows_per_thread = out_rows / num_threads;
-  int extra_rows = out_rows % num_threads;
-  int current_row = 0;
-
-  for (int i = 0; i < num_threads; i++) {
-    int start = current_row;
-    int add = (i < extra_rows) ? 1 : 0;
-    int end = start + rows_per_thread + add;
-
-    thread_data[i].start_row = start;
-    thread_data[i].end_row = end;
-    thread_data[i].out_cols = out_cols;
-    thread_data[i].width = image->width;
-    thread_data[i].height = image->height;
-    thread_data[i].scale = scale;
-    thread_data[i].vscale = vscale;
-    thread_data[i].use_color = use_color;
-    thread_data[i].image = image->data;
-    thread_data[i].gray = gray;
-    thread_data[i].out = output;
-    current_row = end;
-
-    if (pthread_create(&threads[i], NULL, process_rows, &thread_data[i]) != 0)
-      fwprintf(stderr, L"Error create thread %d\n", i);
-  }
-
-  for (int i = 0; i < num_threads; i++)
-    pthread_join(threads[i], NULL);
-
-  for (int i = 0; i < out_rows; i++) {
-    if (output[i]) {
-      wprintf(L"%ls\n", output[i]);
-
-      free(output[i]);
-    }
-  }
-
-  free(output);
-  free(threads);
-  free(thread_data);
-  free(gray);
-
-  stbi_image_free(image->data);
-
-  free(image);
-}
-
-#ifdef __cplusplus
-}
-#endif // __cplusplus
-
-#endif // PIGACO_CONVERTER_H
+#endif // PG_CONVERTER_IMPLEMENTATION
